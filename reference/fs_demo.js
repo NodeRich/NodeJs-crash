2e0bf0fbb719923a4a29e86de55c3ba9b4e1bc14
NodeRich <rahul.rc@outlook.com>
2020-10-19 21:09:24 +0400

    updated fd_demo.js

--- reference/fs_demo.js
+++ reference/fs_demo.js
@@ -1,2 +1,10 @@
 const fs = require('fs');
 const path = require('path');
+
+// create folder
+
+fs.mkdir(path.join(__dirname)+"/test",{},err => {
+ if (err) throw err; 
+ console.log("Folder created");
+ console.log(path.join(__dirname)+"/test");
+});
